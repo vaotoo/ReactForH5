4960a213e66581bbc2a88793165c2a8332fe2ae7
zhaoyang03 <zhaoyang03@kuaishou.com>
2020-01-08 20:35:34 +0800

    引入prettier编码规范

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/**
+ * 格式化配置
+ */
+module.exports = {
+    tabWidth: 4,
+    singleQuote: true,
+    arrowParens: 'always',
+    semi: true,
+    printWidth: 100,
+    trailingComma: 'es5',
+    trailingComma: 'none'
+};
